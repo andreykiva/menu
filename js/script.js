ce34b30f7efc3666962d24fa8b6048516955860a
Andrey <57412446+podmost777@users.noreply.github.com>
2022-05-04 18:14:05 +0300

    all

--- js/script.js
+++ js/script.js
@@ -1,58 +1,42 @@
-let level = 1;
+const $key = document.querySelector(".js_key"),
+	$timer = document.querySelector(".js_timer"),
+	$letters = document.querySelectorAll(".js_letter");
 
-const $scroll1 = document.querySelector(".scroll1"),
-	$scroll2 = document.querySelector(".scroll2"),
-	$scroll3 = document.querySelector(".scroll3"),
-	$scroll4 = document.querySelector(".scroll4"),
-	$scroll5 = document.querySelector(".scroll5"),
-	$answerForm = document.querySelector(".answer-form"),
-	$hideText = document.querySelector(".hide-text"),
-	$outsideInput = document.querySelector(".outside-input");
+const data = "Kings of da Street";
+let letters = 0;
+let sec = 60;
 
-const showNextScroll = (active, next) => {
-	active.classList.remove("scroll-down");
+const intervalId = setInterval(() => {
+	if (sec == 0) {
+		$timer.textContent = "Lose";
+		$timer.classList.add("lose");
+		$key.classList.add("lose");
 
-	setTimeout(() => {
-		active.classList.add("hide");
-		next.classList.remove("hide");
-
-		setTimeout(() => {
-			next.classList.add("scroll-down");
-			next.classList.remove("scroll-up");
-		}, 100);
-	}, 1000);
-};
-
-const chackAnswer = (el) => {
-	if (level === 1) {
-		el.classList.add("showGhost");
-		setTimeout(() => {
-			showNextScroll($scroll1, $scroll2);
-			level++;
-		}, 3000);
-	} else if (level === 2) {
-		if ($answerForm.value.toLowerCase() == "sage") {
-			el.classList.add("showGhost");
-			setTimeout(() => {
-				showNextScroll($scroll2, $scroll3);
-			}, 3000);
-			level++;
-		}
-	} else if (level === 3) {
-		if ($outsideInput.value.toLowerCase() == "baron") {
-			el.classList.add("showGhost");
-			setTimeout(() => {
-				showNextScroll($scroll3, $scroll4);
-			}, 3000);
-			level++;
-		}
-	}
-};
-
-$answerForm.addEventListener("input", (e) => {
-	if (e.target.value.toLowerCase() == "monocle") {
-		$hideText.classList.add("showText");
+		clearTimeout(intervalId);
 	} else {
-		$hideText.classList.remove("showText");
+		$timer.textContent = --sec;
 	}
+}, 1000);
+
+$letters.forEach((letter) => {
+	letter.addEventListener("click", () => {
+		if (letter.classList.contains("show")) return;
+
+		letter.classList.add("show");
+		letters++;
+
+		if (data[letters - 1] == " ") {
+			letters++;
+		}
+
+		$key.textContent = data.slice(0, letters);
+
+		if (letters === data.length) {
+			$timer.textContent = "Win";
+			$timer.classList.add("win");
+			$key.classList.add("win");
+
+			clearTimeout(intervalId);
+		}
+	});
 });
